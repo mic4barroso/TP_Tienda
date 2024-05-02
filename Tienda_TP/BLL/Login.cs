9092ec8f159d1e96f17628d5cd6046fbbd6792c0
mic4barroso <113726795+mic4barroso@users.noreply.github.com>
2024-05-02 13:17:56 -0300

    Add files via upload

---
+++ Tienda_TP/BLL/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Login
+    {
+        private string _username = "username";
+        private string _password = "password";
+
+        /*public bool IniciarSesion(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }*/
+
+        public bool IniciarSesion(string username, string password) {
+
+            if (string.IsNullOrEmpty(username)) {
+                throw new ArgumentNullException("username");
+            }
+
+            if (string.IsNullOrEmpty(password)) {  
+                throw new ArgumentNullException("password"); 
+            }
+
+            if(_username == username || _password == password) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tienda_TP/BLL/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public abstract class Usuario
+    {
+		private string _nombre;
+        private string _apellido;
+
+        public string Nombre
+		{
+			get { return _nombre; }
+			set { _nombre = value; }
+		}
+
+		public string Apellido
+		{
+			get { return _apellido; }
+			set { _apellido = value; }
+		}
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string nombre, string apellido)
+        {
+              this.Nombre = nombre;
+              this.Apellido = apellido;
+        }
+
+    }
+}
