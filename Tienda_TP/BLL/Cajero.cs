c87f95e26b3ae74291ee8075c1d5ab971569d95b
mic4barroso <113726795+mic4barroso@users.noreply.github.com>
2024-05-03 12:53:50 -0300

    Add files via upload

---
+++ Tienda_TP/BLL/Cajero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Cajero : Usuario
+    {
+        public Cajero(int Id, string nombre, string apellido, string user, string contraseña) : base(Id, nombre, apellido, user, contraseña)
+        {
+        }
+
+        public override string ObtenerRol(string elRol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
