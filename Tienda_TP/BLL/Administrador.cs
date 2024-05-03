1d962993f67e58fde8416442382dce507a2cb7fd
mic4barroso <113726795+mic4barroso@users.noreply.github.com>
2024-05-03 10:43:28 -0300

    Add files via upload

---
+++ Tienda_TP/BLL/Administrador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    //Administrador hereda de Usuario que es una CLASE ABSTRACTA
+    public class Administrador : Usuario
+    {
+        public Administrador(int Id, string nombre, string apellido, string user, string contraseña) : base(Id, nombre, apellido, user, contraseña)
+        {
+        }
+        public override string ObtenerRol() => "Administrador";
+    }
+}
---
+++ Tienda_TP/BLL/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Login
+    {
+        //hardcodee para probar si funciona
+        private List<Usuario> usuarios = new List<Usuario>()
+        {
+            new Administrador(1, "Ana", "Ruescas", "anaAdmin", "passwordAna"),
+            new Administrador(2, "Ciro", "Coria", "ciroAdmin", "passwordCiro"),
+            new Administrador(3, "Eric", "Perez", "ericAdmin", "passwordEric"),
+            new Administrador(4, "Mica", "Barroso", "micaAdmin", "passwordMica")
+        };
+
+        public bool IniciarSesion(string username, string password)
+        {
+
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException("username");
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("password");
+            }
+
+            //Busca coincidencias 
+            var usuario = usuarios.FirstOrDefault(u => u.User == username && u.Contraseña == password);
+            return usuario != null;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+   
